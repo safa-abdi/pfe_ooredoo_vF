ac58042b5fc87231d071e8ef650dda145c8bccfb
Abdi Safa <safaabdi930@gmail.com>
2025-05-17 01:59:37 +0100

    Create activation.module.ts

---
+++ back_tachyon/src/activation/activation.module.ts
@@ -0,0 +1,52 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Activation } from './entities/activation.entity';
+import { Company } from '../companies/entities/company.entity';
+import { Branch } from '../branches_companies/entities/Branch.entity';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ScheduleModule } from '@nestjs/schedule';
+
+// Contrôleurs
+import { ActivationController } from './activation.controller';
+import { ActivationSlaController } from './sla.controller';
+
+// Services
+import { ActivationService } from './activation.service';
+import { ActivationBulkService } from './activation-bulk.service';
+import { ActivationCronService } from './activation.cron.service';
+import { CompanyDelegationModule } from 'src/branches_companies/company-delegation.module';
+import { DelegationModule } from 'src/branches_companies/delegation.module';
+import { Prod_imei } from './entities/Prod_imei.entity';
+import { PdfController } from './pv.controller';
+import { CacheService } from 'src/cache/cache.service';
+import { History } from 'src/history/entities/history.entity';
+import { HistoryService } from 'src/history/history.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Activation, Company, Branch, Prod_imei, History]),
+    CacheModule.register({
+      ttl: 60 * 60 * 1, // Cache pour 1 heure
+    }),
+    ScheduleModule.forRoot(),
+    CompanyDelegationModule,
+    DelegationModule,
+  ],
+  controllers: [ActivationController, ActivationSlaController, PdfController],
+  providers: [
+    ActivationService,
+    ActivationBulkService,
+    ActivationCronService,
+    HistoryService,
+    {
+      provide: 'SLA_CONFIG',
+      useValue: {
+        batchSize: 1000,
+        timeout: 30000,
+      },
+    },
+    CacheService,
+  ],
+  exports: [ActivationService, TypeOrmModule],
+})
+export class ActivationModule {}
